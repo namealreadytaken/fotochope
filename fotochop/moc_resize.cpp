d0c4820dcf71865f66ad5030e95b2e6a87fbea3b
vv.stockage <vv.stockage@gmail.com>
2013-01-11 13:42:11 +0000

    finalisation de la fenetre de resize load image a améliorer pour qu'elle redimensionne la panel et la scrol area

--- fotochop/moc_resize.cpp
+++ fotochop/moc_resize.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'resize.h'
 **
-** Created: Fri 11. Jan 13:24:39 2013
+** Created: Fri 11. Jan 14:39:52 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,25 +23,36 @@ static const uint qt_meta_data_Resize[] = {
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: signature, parameters, type, tag, flags
+       8,    7,    7,    7, 0x0a,
+      34,    7,    7,    7, 0x0a,
+
        0        // eod
 };
 
 static const char qt_meta_stringdata_Resize[] = {
-    "Resize\0"
+    "Resize\0\0on_cancelButton_clicked()\0"
+    "on_okButton_clicked()\0"
 };
 
 void Resize::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        Resize *_t = static_cast<Resize *>(_o);
+        switch (_id) {
+        case 0: _t->on_cancelButton_clicked(); break;
+        case 1: _t->on_okButton_clicked(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -76,6 +87,11 @@ int Resize::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QDialog::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
