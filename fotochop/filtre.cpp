70e12f9b784e738dcf12a566a4f34667d82bb87d
s.samy38 <s.samy38@gmail.com>
2013-01-26 20:46:46 +0000

    ajout du gradient

--- fotochop/filtre.cpp
+++ fotochop/filtre.cpp
@@ -24,21 +24,37 @@ Filtre::~Filtre() {
 void Filtre::on_okButton_clicked() {
     int Filtre[3][3];
     int div;
-    Filtre[0][0] = widget.l1->text().toInt();
-    Filtre[0][1] = widget.l2->text().toInt();
-    Filtre[0][2] = widget.l3->text().toInt();
-    Filtre[1][0] = widget.l4->text().toInt();
-    Filtre[1][1] = widget.l5->text().toInt();
-    Filtre[1][2] = widget.l6->text().toInt();
-    Filtre[2][0] = widget.l7->text().toInt();
-    Filtre[2][1] = widget.l8->text().toInt();
-    Filtre[2][2] = widget.l9->text().toInt();
-    div = widget.diviseur->text().toInt();
-    pere->filtrer(Filtre, div);
+    if (widget.l1->isEnabled()) {
+        Filtre[0][0] = widget.l1->text().toInt();
+        Filtre[0][1] = widget.l2->text().toInt();
+        Filtre[0][2] = widget.l3->text().toInt();
+        Filtre[1][0] = widget.l4->text().toInt();
+        Filtre[1][1] = widget.l5->text().toInt();
+        Filtre[1][2] = widget.l6->text().toInt();
+        Filtre[2][0] = widget.l7->text().toInt();
+        Filtre[2][1] = widget.l8->text().toInt();
+        Filtre[2][2] = widget.l9->text().toInt();
+        div = widget.diviseur->text().toInt();
+        pere->filtrer(Filtre, div);
+    } else {
+        pere->Sobel();
+    }
     this->close();
 }
 
 void Filtre::on_presets_activated(int i) {
+    if (!widget.l1->isEnabled()) {
+        widget.l1->setEnabled(true);
+        widget.l2->setEnabled(true);
+        widget.l3->setEnabled(true);
+        widget.l4->setEnabled(true);
+        widget.l5->setEnabled(true);
+        widget.l6->setEnabled(true);
+        widget.l7->setEnabled(true);
+        widget.l8->setEnabled(true);
+        widget.l9->setEnabled(true);
+        widget.diviseur->setEnabled(true);
+    }
     if (i == 0) {
         widget.l1->setText("0");
         widget.l2->setText("0");
@@ -73,15 +89,15 @@ void Filtre::on_presets_activated(int i) {
         widget.l9->setText("0");
         widget.diviseur->setText("5");
     } else if (i == 3) {
-        widget.l1->setText("0");
-        widget.l2->setText("0");
-        widget.l3->setText("0");
-        widget.l4->setText("0");
-        widget.l5->setText("1");
-        widget.l6->setText("0");
-        widget.l7->setText("0");
-        widget.l8->setText("0");
-        widget.l9->setText("0");
-        widget.diviseur->setText("1");
+        widget.l1->setEnabled(false);
+        widget.l2->setEnabled(false);
+        widget.l3->setEnabled(false);
+        widget.l4->setEnabled(false);
+        widget.l5->setEnabled(false);
+        widget.l6->setEnabled(false);
+        widget.l7->setEnabled(false);
+        widget.l8->setEnabled(false);
+        widget.l9->setEnabled(false);
+        widget.diviseur->setEnabled(false);
     }
 }
